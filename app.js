472b06af1d1047e0ea0a9c6a8417fa7cacd5d9d8
Igor <ichaves@live.com>
2023-02-22 13:39:52 +0900

    Added removeItem function
    - created items can be delete through delete button

--- app.js
+++ app.js
@@ -1,31 +1,40 @@
 const transactionsForm = document.querySelector('[data-js="transactions-form"]')
 const transactionsList = document.querySelector('[data-js="transactions-list"]')
-// const 
 
 let transactions = []
 
+const removeItem = item => {
+   transactions = transactions.filter(transaction => transaction.description !== item)
+   console.log(transactions)
+}
+
+const addItemIntoDOM = transaction => {
+   transactions.push(transaction)
+}
+
 transactionsForm.addEventListener("submit", e => {
    e.preventDefault()
-   
+
+   transactionsList.innerHTML = ""
+
    const name = transactionsForm.description.value
    const amount = transactionsForm.amount.value
    const transaction = {"id": 01, "description": name, "amount": Number(amount)}
    
-   transactions.push(transaction)
-   console.log(transactions)
-   
    const li = document.createElement("li")
    transactionsList.prepend(li)
+
+   addItemIntoDOM(transaction)
+   console.log(transactions)
 
    li.innerHTML += `
    <span data-js="description">${name}</span>
    <span>U$ ${amount}</span>
-   <button onClick="removeItem(${transaction.description})">X</button>
+   <button data-js="remove-button">X</button>
    `
-   
-})
 
-// const removeItem = item => {
-//    transactions = transactions.filter(transaction => {transaction.description !== item})
-//    console.log(transactions)
-// }
+   const removeBtn = document.querySelector('[data-js="remove-button"]')
+   removeBtn.addEventListener('click', () => {
+      removeItem(transaction.description)
+   })
+})
